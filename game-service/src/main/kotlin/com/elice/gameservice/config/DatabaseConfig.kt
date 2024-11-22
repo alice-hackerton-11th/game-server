64ab41cf00dec30554c3154ebd1b8e11e154c432
diydriller <dhrhd080@naver.com>
2024-11-22 22:11:35 +0900

    feat: 방 조회 메서드 추가

---
+++ game-service/src/main/kotlin/com/elice/gameservice/config/DatabaseConfig.kt
@@ -0,0 +1,9 @@
+package com.elice.gameservice.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class DatabaseConfig {
+}
