e87dc7d42340b9e7c6fdc9b46447e6a8b6754530
diydriller <dhrhd080@naver.com>
2024-11-22 20:38:14 +0900

    feat: roomMember 엔티티에  방 입장/퇴장 메서드 추가

--- game-service/src/main/kotlin/com/elice/gameservice/domain/room/model/RoomMember.kt
+++ game-service/src/main/kotlin/com/elice/gameservice/domain/room/model/RoomMember.kt
@@ -1,7 +1,10 @@
 package com.elice.gameservice.domain.room.model
 
+import com.elice.common.exception.ConflictException
+import com.elice.common.response.BaseResponseStatus
+import com.elice.gameservice.domain.common.enums.DeleteState
 import com.elice.gameservice.domain.common.model.BaseModel
-import com.elice.gameservice.domain.common.value.RoomMemberEnteredState
+import com.elice.gameservice.domain.room.enums.RoomMemberEnteredState
 import jakarta.persistence.*
 
 @Entity
@@ -18,4 +21,20 @@ class RoomMember(
 
     @Enumerated(value = EnumType.STRING)
     var roomMemberEnteredState: RoomMemberEnteredState = RoomMemberEnteredState.NOT_ENTERED
+
+    fun enterRoom() {
+        roomMemberEnteredState = RoomMemberEnteredState.ENTERED
+        room.memberCount++
+        if (room.memberCount > room.memberLimit) {
+            throw ConflictException(BaseResponseStatus.EXCEED_MEMBER_LIMIT)
+        }
+    }
+
+    fun exitRoom() {
+        roomMemberEnteredState = RoomMemberEnteredState.NOT_ENTERED
+        room.memberCount--
+        if (room.memberCount <= 0) {
+            room.deleteState = DeleteState.DELETED
+        }
+    }
 }
