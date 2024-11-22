5f686ab12cb24b66a59099d937c0007c2725186c
diydriller <dhrhd080@naver.com>
2024-11-22 20:35:25 +0900

    fix: room의 시작상태를 room 엔티티의 필드로 추가 & 멤버 수와 삭제여부 필드 추가

--- game-service/src/main/kotlin/com/elice/gameservice/domain/room/model/Room.kt
+++ game-service/src/main/kotlin/com/elice/gameservice/domain/room/model/Room.kt
@@ -1,7 +1,9 @@
 package com.elice.gameservice.domain.room.model
 
+import com.elice.gameservice.domain.common.enums.DeleteState
 import com.elice.gameservice.domain.common.model.BaseModel
-import com.elice.gameservice.domain.common.value.RoomMemberEnteredState
+import com.elice.gameservice.domain.room.enums.RoomMemberEnteredState
+import com.elice.gameservice.domain.room.enums.RoomProgressState
 import jakarta.persistence.*
 
 @Entity
@@ -15,12 +17,24 @@ class Room(
     @Column(name = "room_id")
     var id: Long? = null
 
+    var memberCount: Long = 0
+
     @OneToMany(mappedBy = "room", cascade = [CascadeType.PERSIST])
     var roomMemberList: MutableList<RoomMember> = ArrayList()
+
+    @Enumerated(EnumType.STRING)
+    var deleteState: DeleteState = DeleteState.NOT_DELETED
+
+    @Enumerated(value = EnumType.STRING)
+    var roomProgressState: RoomProgressState = RoomProgressState.NOT_STARTED
+
+    @Version
+    var version: Long? = null
 
     fun createRoomMember(memberId: Long) {
         val roomMember = RoomMember(this, memberId)
         roomMember.roomMemberEnteredState = RoomMemberEnteredState.ENTERED
         roomMemberList.add(roomMember)
+        memberCount++
     }
 }
--- game-service/src/main/kotlin/com/elice/gameservice/domain/room/model/RoomMemberInfo.kt
+++ game-service/src/main/kotlin/com/elice/gameservice/domain/room/model/RoomMemberInfo.kt
@@ -1,16 +1,13 @@
 package com.elice.gameservice.domain.room.model
 
-import com.elice.gameservice.domain.common.value.RoomMemberProgressState
-import com.elice.gameservice.domain.common.value.RoomMemberRole
+import com.elice.gameservice.domain.room.enums.RoomMemberRole
 import jakarta.persistence.*
 
 @Entity
 class RoomMemberInfo(
     @Enumerated(value = EnumType.STRING)
     val roomMemberRole: RoomMemberRole,
-    var heart: Int,
-    @Enumerated(value = EnumType.STRING)
-    var roomMemberProgressState: RoomMemberProgressState = RoomMemberProgressState.IN_PROGRESS
+    var heart: Int
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
